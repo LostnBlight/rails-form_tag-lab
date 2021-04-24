594837aac1175950c9992cf0aa780dcdaccd9d65
Your Name <you@example.com>
2021-04-23 19:32:36 -0600

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,8 +4,20 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  
+  # -----Creating students for Controller action-----
+  
+  def create 
+    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to students_path
+  end
+
   def show
     @student = Student.find(params[:id])
+  end
+
+  def new
+
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :students, only: :index
+
+  #Create is to create the user as new is do show a page for the form 
+  resources :students, only: [:index, :create, :new]
+
   get '/students/:id', to: 'students#show', as: 'student'
 end
